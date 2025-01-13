cee1552379d3b343c7702583568dc14c15d275ad
Thomas <tienhau.it@gmail.com>
2025-01-13 10:16:19 +0700

    Create: Tạo thanh bottom navigation

---
+++ Application/app/src/main/java/com/application/application/activity/sale/SaleActivity.java
@@ -0,0 +1,15 @@
+package com.application.application.activity.sale;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.application.application.R;
+
+public class SaleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
